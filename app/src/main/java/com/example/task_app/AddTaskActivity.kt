f69e578d35e945228fc1dc47df5f1c1f72d0ee11
JANINDU DULANJITH JAYASUNDARA <127662973+JaninduDulanjith@users.noreply.github.com>
2024-05-13 07:30:22 +0530

    edit

---
+++ app/src/main/java/com/example/task_app/AddTaskActivity.kt
@@ -0,0 +1,26 @@
+package com.example.task_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.task_app.databinding.ActivityAddTaskBinding
+class AddTaskActivity : AppCompatActivity() {
+    private  lateinit var binding: ActivityAddTaskBinding
+    private  lateinit var db: TaskDatabaseHelper
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = TaskDatabaseHelper(this)
+
+        binding.saveButton.setOnClickListener{
+            val title = binding.titleEditText.text.toString()
+            val content = binding.ContentText.text.toString()
+            val task = Task(0, title, content)
+            db.insertTask(task)
+            finish()
+            Toast.makeText(this, "Task Saved", Toast.LENGTH_SHORT).show()
+            }
+    }
+}
--- app/src/main/java/com/example/task_app/MainActivity.kt
+++ app/src/main/java/com/example/task_app/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.task_app
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.task_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var db: TaskDatabaseHelper
+    private lateinit var taskAdapter: TaskAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = TaskDatabaseHelper(this)
+        taskAdapter = TaskAdapter(db.getAllTasks(), this)
+
+        binding.taskRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.taskRecyclerView.adapter = taskAdapter
+
+        binding.addButton.setOnClickListener{
+            val intent = Intent(this,AddTaskActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        taskAdapter.refreshData(db.getAllTasks())
     }
 }
